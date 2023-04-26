317f13670974395c4210213aa94622d5731fcf5c
bogo8liuk <lucaborghi99@gmail.com>
2023-04-26 19:16:19 +0200

    types redefinition

--- src/Compiler/Config/Types.hs
+++ src/Compiler/Config/Types.hs
@@ -37,6 +37,7 @@ module Compiler.Config.Types
 
 import Data.Array
 import GHC.Exts(maxTupleSize)
+import Compiler.Config.Rep
 import qualified Compiler.Config.Lexer as Lexer
 
 data SizeType = Fixed Int | Variable
@@ -44,14 +45,14 @@ data SizeType = Fixed Int | Variable
 data Type =
     BT
         { source :: String
-        , name :: String
+        , name :: TokenRep
         }
 
-nameInt :: String
-nameInt = "Int"
+nameInt :: TokenRep
+nameInt = tokenRepFromStr "Int"
 
 sourceInt :: String
-sourceInt = Lexer.adtKeyword ++ " " ++ nameInt ++ "\n"
+sourceInt = Lexer.adtKeyword ++ " " ++ tokenRepToStr nameInt ++ "\n"
 
 int :: Type
 int =
@@ -60,11 +61,11 @@ int =
         , name = nameInt
         }
 
-nameNat :: String
-nameNat = "Nat"
+nameNat :: TokenRep
+nameNat = tokenRepFromStr "Nat"
 
 sourceNat :: String
-sourceNat = Lexer.adtKeyword ++ " " ++ nameNat ++ "\n"
+sourceNat = Lexer.adtKeyword ++ " " ++ tokenRepToStr nameNat ++ "\n"
 
 nat :: Type
 nat =
@@ -73,11 +74,11 @@ nat =
         , name = nameNat
         }
 
-nameDouble :: String
-nameDouble = "Double"
+nameDouble :: TokenRep
+nameDouble = tokenRepFromStr "Double"
 
 sourceDouble :: String
-sourceDouble = Lexer.adtKeyword ++ " " ++ nameDouble ++ "\n"
+sourceDouble = Lexer.adtKeyword ++ " " ++ tokenRepToStr nameDouble ++ "\n"
 
 double :: Type
 double =
@@ -86,22 +87,22 @@ double =
         , name = nameDouble
         }
 
-nameBool :: String
-nameBool = "Bool"
+nameBool :: TokenRep
+nameBool = tokenRepFromStr "Bool"
 
-trueConBool :: String
-trueConBool = Lexer.trueCon
+trueConBool :: TokenRep
+trueConBool = tokenRepFromStr Lexer.trueCon
 
-falseConBool :: String
-falseConBool = Lexer.falseCon
+falseConBool :: TokenRep
+falseConBool = tokenRepFromStr Lexer.falseCon
 
-boolCons :: [String]
+boolCons :: [TokenRep]
 boolCons = [trueConBool, falseConBool]
 
 sourceBool :: String
 sourceBool =
-    Lexer.adtKeyword ++ " " ++ nameBool ++ " " ++ Lexer.definitionKeyword ++ " " ++ trueConBool ++ " " ++
-    Lexer.caseSeparationKeyword ++ " " ++ falseConBool ++ "\n"
+    Lexer.adtKeyword ++ " " ++ tokenRepToStr nameBool ++ " " ++ Lexer.definitionKeyword ++ " " ++ Lexer.trueCon ++
+        " " ++ Lexer.caseSeparationKeyword ++ " " ++ Lexer.falseCon ++ "\n"
 
 bool :: Type
 bool =
@@ -110,11 +111,11 @@ bool =
         , name = nameBool
         }
 
-nameChar :: String
-nameChar = "Char"
+nameChar :: TokenRep
+nameChar = tokenRepFromStr "Char"
 
 sourceChar :: String
-sourceChar = Lexer.adtKeyword ++ " " ++ nameChar ++ "\n"
+sourceChar = Lexer.adtKeyword ++ " " ++ tokenRepToStr nameChar ++ "\n"
 
 char :: Type
 char =
@@ -123,11 +124,11 @@ char =
         , name = nameChar
         }
 
-nameByteString :: String
-nameByteString = "String"
+nameByteString :: TokenRep
+nameByteString = tokenRepFromStr "String"
 
 sourceByteString :: String
-sourceByteString = Lexer.adtKeyword ++ " " ++ nameByteString ++ "\n"
+sourceByteString = Lexer.adtKeyword ++ " " ++ tokenRepToStr nameByteString ++ "\n"
 
 byteString :: Type
 byteString =
@@ -136,11 +137,11 @@ byteString =
         , name = nameByteString
         }
 
-nameFunctionApp :: String
-nameFunctionApp = "App"
+nameFunctionApp :: TokenRep
+nameFunctionApp = tokenRepFromStr "App"
 
 sourceFunctionApp :: String
-sourceFunctionApp = Lexer.adtKeyword ++ " " ++ nameFunctionApp ++ " a b\n"
+sourceFunctionApp = Lexer.adtKeyword ++ " " ++ tokenRepToStr nameFunctionApp ++ " a b\n"
 
 functionApp :: Type
 functionApp =
@@ -149,21 +150,21 @@ functionApp =
         , name = nameFunctionApp
         }
 
-nameList :: String
-nameList = "List"
+nameList :: TokenRep
+nameList = tokenRepFromStr "List"
 
-emptyConList :: String
-emptyConList = Lexer.listEmptyCon
+emptyConList :: TokenRep
+emptyConList = tokenRepFromStr Lexer.listEmptyCon
 
-consConList :: String
-consConList = Lexer.listConsCon
+consConList :: TokenRep
+consConList = tokenRepFromStr Lexer.listConsCon
 
-listCons :: [String]
+listCons :: [TokenRep]
 listCons = [emptyConList, consConList]
 
 --TODO: add cons to the source
 sourceList :: String
-sourceList = Lexer.adtKeyword ++ " " ++ nameList ++ " a\n"
+sourceList = Lexer.adtKeyword ++ " " ++ tokenRepToStr nameList ++ " a\n"
 
 list :: Type
 list =
@@ -172,41 +173,43 @@ list =
         , name = nameList
         }
 
-nameTuple :: String
-nameTuple = "Tuple"
+nameTuple :: TokenRep
+nameTuple = tokenRepFromStr "Tuple"
 
-namesTuple :: Array Int String
-namesTuple = array (2, maxTupleSize) [ (i, nameTuple ++ show i) | i <- [2..maxTupleSize] ]
+namesTuple :: Array Int TokenRep
+namesTuple =
+    array (2, maxTupleSize) [ (i, tokenRepFromStr (tokenRepToStr nameTuple ++ show i)) | i <- [2..maxTupleSize] ]
 
 {- A 2-maxTupleSize array of tuple constructors. -}
-conTuples :: Array Int String
+conTuples :: Array Int TokenRep
 conTuples = array (2, maxTupleSize) [ (i, mkTupleCon i) | i <- [2..maxTupleSize] ]
 
-conTuplesList :: [(Int, String)]
+conTuplesList :: [(Int, TokenRep)]
 conTuplesList = assocs conTuples
 
-mkTupleCon :: Int -> String
+mkTupleCon :: Int -> TokenRep
 mkTupleCon i =
-    Lexer.tupleConSugarStart ++
-    concat [ Lexer.tupleConSugarSep | _ <- [1..(i - 1)] ] ++
-    Lexer.tupleConSugarEnd
+    tokenRepFromStr $
+        Lexer.tupleConSugarStart ++
+        concat [ Lexer.tupleConSugarSep | _ <- [1..(i - 1)] ] ++
+        Lexer.tupleConSugarEnd
 
-getTupleCon :: Int -> Maybe String
+getTupleCon :: Int -> Maybe TokenRep
 getTupleCon i =
     let (minTup, maxTup) = bounds conTuples in
         if i < minTup || i > maxTup
         then Nothing
         else Just $ namesTuple ! i
 
-mkTupleParams :: Int -> String
-mkTupleParams i = concat [ " e" ++ show j | j <- [1..i] ]
+mkTupleParams :: Int -> [TokenRep]
+mkTupleParams i = [ tokenRepFromStr ("e" ++ show j) | j <- [1..i] ]
 
 mkTupleSource :: Int -> String
 mkTupleSource i =
-    let params = mkTupleParams i in
-    let con = conTuples ! i in
-    let name = namesTuple ! i in
-        Lexer.adtKeyword ++ " " ++ name ++ params ++ " " ++ Lexer.definitionKeyword ++ " " ++ con ++ params ++ "\n"
+    let params = concatMap (\p -> " " ++ tokenRepToStr p) $ mkTupleParams i in
+    let con = tokenRepToStr $ conTuples ! i in
+    let tyCon = tokenRepToStr $ namesTuple ! i in
+        Lexer.adtKeyword ++ " " ++ tyCon ++ params ++ " " ++ Lexer.definitionKeyword ++ " " ++ con ++ params ++ "\n"
 
 tuples :: [Type]
 tuples =
