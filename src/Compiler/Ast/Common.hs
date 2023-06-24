edc117f631f4349b5d900cb9b3bd7c33dd0d6142
bogo8liuk <lucaborghi99@gmail.com>
2023-06-24 12:06:14 +0200

    first refactoring: itemization, representation, ids

--- src/Compiler/Ast/Common.hs
+++ src/Compiler/Ast/Common.hs
@@ -1,20 +1,86 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{- |
+Module : Compiler.Ast.Common
+Description : Common interfaces for ast
+Copyright : (c) Luca Borghi, 2022
+License : GPL-3
+Stability : experimental
 
-{- Common interfaces and functions among different representations of ast. -}
+Common interfaces for the abstract syntax tree, regardless the nature of the tree (typed, untyped or something else).
+Some notation: we will refer to nodes of the ast as "tokens", for example, a type definition (with presumably a name
+and list of constructors) is a token; we will refer to opaque objects denoting some tokens as "items". An item should
+keep no information about a token, including its structure and it shouldn't be used as an identifier for the token.
+-}
+
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Compiler.Ast.Common
-    ( module Compiler.Config.Rep
-    , HasArgs(..)
-    , HasHead(..)
-    , HasState(..)
-    , AtomRep(..)
-    , strOf
-    , Binder(..)
-    , Diff(..)
-    , CxtDiff(..)
+    (
+    --( module Compiler.Config.Rep
+    --, HasArgs(..)
+    --, HasHead(..)
+    --, HasState(..)
+    --, AtomRep(..)
+    --, strOf
+    --, Binder(..)
+    --, Diff(..)
+    --, CxtDiff(..)
+      Item(..)
+    , TokenRep
+    , HasRepresentation(..)
+    , textOf
+    , Id(..)
+    , HasId(..)
 ) where
 
-import Compiler.Config.Rep
+import Utils.TypeAlias (Typing (unTyping))
+import Data.Text (Text)
 
+{- |
+The class for "itemization" of tokens.
+-}
+class Item token item where
+    {- |
+    It returns the item of a token.
+    -}
+    itemOf :: token -> item
+
+data RepresentationType
+type TokenRep = RepresentationType `Typing` Text
+
+{- |
+The class for tokens which can be represented as `TokenRep`.
+-}
+class HasRepresentation token where
+    representationOf :: token -> TokenRep
+
+    rawRepresentationOf :: token -> Text
+    rawRepresentationOf = unTyping . representationOf
+
+textOf :: TokenRep -> Text
+textOf = unTyping
+
+data Id
+    -- | A program variable
+    = VarId !TokenRep
+    | DataConId !TokenRep
+    | TypeConId !TokenRep
+    | TypeVarId !TokenRep
+    | KindConId !TokenRep
+    | KindVarId !TokenRep
+    -- | A constructor coming from a type class
+    | ConstrConId !TokenRep
+    -- | Identifier for tokens solved at compile-time
+    | CompTimeTokenId !TokenRep
+
+{- |
+Tokens which can be identified by an `Id`.
+-}
+class HasId token where
+    idOf :: token -> Id
+
+{-
 -- Type-classes: HasArgs
 
 {- Type-class for those tokens which have the notion of arguments. -}
@@ -60,3 +126,4 @@ class CxtDiff tok cxt m where
     cxtMinus :: tok -> tok -> cxt -> m tok
     cxtMinus t1 t2 = cxtDiff t1 [t2]
     cxtDiff :: tok -> [tok] -> cxt -> m tok
+    -}
