950e10fd8c2ef614866ab9c3cf2ca405e60e2a01
bogo8liuk <lucaborghi99@gmail.com>
2023-09-09 07:44:37 +0200

    parsing names

--- src/Compiler/Syntax/Refactoring/Grammar.hs
+++ src/Compiler/Syntax/Refactoring/Grammar.hs
@@ -10,26 +10,60 @@ Parsing of language constructs.
 module Compiler.Syntax.Refactoring.Grammar
     (
 ) where
+
 import Compiler.Syntax.Refactoring.Types (LangParser)
 import qualified Compiler.Ast.Raw.Tree as Raw
+import Compiler.Lib.Pos as With (ProgramPos)
+import Compiler.Syntax.Refactoring.Lib (track)
+import Compiler.Syntax.Refactoring.Language (lowerIdentifier, upperIdentifier)
+import Data.Text (Text)
 
-declaration :: LangParser (Raw.Declaration a)
+declaration :: LangParser (Raw.Declaration With.ProgramPos)
 declaration = undefined
 
-typeDefinition :: LangParser (Raw.TypeDefinition a)
+typeDefinition :: LangParser (Raw.TypeDefinition With.ProgramPos)
 typeDefinition = undefined
 
-aliasDefinition :: LangParser (Raw.AliasDefinition a)
+aliasDefinition :: LangParser (Raw.AliasDefinition With.ProgramPos)
 aliasDefinition = undefined
 
-typeClassDefinition :: LangParser (Raw.TypeClassDefinition a)
+typeClassDefinition :: LangParser (Raw.TypeClassDefinition With.ProgramPos)
 typeClassDefinition = undefined
 
-instanceDefinition :: LangParser (Raw.InstanceDefinition a)
+instanceDefinition :: LangParser (Raw.InstanceDefinition With.ProgramPos)
 instanceDefinition = undefined
 
-signatureDeclaration :: LangParser (Raw.Signature a)
+signatureDeclaration :: LangParser (Raw.Signature With.ProgramPos)
 signatureDeclaration = undefined
 
-bindingDeclaration :: LangParser (Raw.Binding a)
+bindingDeclaration :: LangParser (Raw.Binding With.ProgramPos)
 bindingDeclaration = undefined
+
+symbolName :: LangParser (Raw.SymbolName With.ProgramPos)
+symbolName = parseName Lower Raw.SymName
+
+typeConName :: LangParser (Raw.TypeConName With.ProgramPos)
+typeConName = parseName Upper Raw.TyConName
+
+dataConName :: LangParser (Raw.DataConName With.ProgramPos)
+dataConName = parseName Upper Raw.DataConName
+
+constraintName :: LangParser (Raw.ConstraintName With.ProgramPos)
+constraintName = parseName Upper Raw.ConstrName
+
+typeVarName :: LangParser (Raw.TypeVarName With.ProgramPos)
+typeVarName = parseName Lower Raw.TyVarName
+
+data LetterCase = Lower | Upper
+
+parseName
+    :: Functor name
+    => LetterCase
+    -> (Text -> () -> name ())
+    -> LangParser (name With.ProgramPos)
+parseName letterCase build = track $ do
+    textName <-
+        case letterCase of
+            Lower -> lowerIdentifier
+            Upper -> upperIdentifier
+    return $ build textName ()
